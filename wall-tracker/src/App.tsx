14376984a92dfca1aa92d5f76125bcb1b91e9d93
JonasUnruh <Jonas.unruh@t-online.de>
2022-02-23 15:24:37 +0100

    Button stellt get Anfrage an API und stellt alle Boulder auf WEbsite dar

--- wall-tracker/src/App.tsx
+++ wall-tracker/src/App.tsx
@@ -1,16 +1,47 @@
-import {Button, List, ListItem, TextField} from '@material-ui/core'
-import React from 'react'
+import {Button, List, ListItem, TextField} from '@material-ui/core';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-    let [text, changeText] = React.useState("");
-  return (
-    <div>
-      <A></A>
-    </div>
-  );
+    const [boulders, setBoulders] = useState([{id: 0, difficulty: 0}]);
+    const boulderApiUrl = "http://localhost:8080/boulder";
+
+    const fetchData = async () => {
+        const response = await axios.get(boulderApiUrl)
+        setBoulders(response.data)
+    }
+
+    return (
+        <div className="App">
+            <h1>Boulders</h1>
+            <h2>Fetch a List of all Boulders in the Database</h2>
+
+            {/*Fetch the data*/}
+            <div>
+                <button className="fetchButton" onClick={fetchData}>
+                    Fetch Boulders
+                </button>
+            </div>
+
+            {/*Display Boulders*/}
+            <div className="boulders">
+                {boulders[0].id != 0  &&
+                    boulders.map((boulder, index) => {
+                    const difficulty = boulder.difficulty
+
+                    return (
+                        <div className="boulder" key={index}>
+                            <h3>Boulder {index + 1}</h3>
+                            <h4>Difficulty: {difficulty}</h4>
+                        </div>
+                    );
+                    })
+                }
+            </div>
+        </div>
+    );
 }
-
-
 
 interface IState {
     boulders?: [{id: number, difficulty: number}];
@@ -27,31 +58,32 @@ class A extends React.Component<IProps, IState> {
         this.state = {boulders: [{id: 0, difficulty: 0}]}
     }
 
-    componentDidMount() {
-        fetch("http://localhost:8080/boulder")
-            .then(response => response.json())
-            .then(val => this.setState({boulders: val}))
-            .catch(() => {})
+    async getBoulders() {
+        const response = await fetch("http://localhost:8080/boulder");
+        const data = await response.json();
+        this.setState({boulders: data})
     }
+
 
     render() {
         return (
             <div>
                 <h1 style={{textAlign: "center"}}>Hallo jonas</h1>
                 <h2>Penis</h2>
+                <div style={{padding: 16, margin: 16}}>
+                    <Button variant={"contained"}
+                            onClick={this.getBoulders}>
+                        Boulders
+                    </Button>
+                </div>
                 <div>
                     <List>
                         {this.state.boulders!.map(boulder => <ListItem><span>{boulder.id} {boulder.difficulty}</span></ListItem>)}
                     </List>
-                </div>
-                <div style={{padding: 16, margin: 16}}>
-                    <Button variant={"contained"} onClick={() => {
-
-                    }}>Boulders</Button>
                 </div>
             </div>
         )
     }
 }
 
-export default A;
+export default App;
