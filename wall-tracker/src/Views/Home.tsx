c4a2246d307be8262651cdc019d5d7220a7b75ad
JonasUnruh <Jonas.unruh@t-online.de>
2022-03-08 20:18:18 +0100

    Interface and Home changes

--- wall-tracker/src/Views/Home.tsx
+++ wall-tracker/src/Views/Home.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import {Avatar, Box, IconButton} from "@mui/material";
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 
 const Home = () => {
     return(
-        <div>
-            <h1>Home Page</h1>
+        <div style={{ display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: "100vh"
+        }}
+        >
+            <Box
+                sx={{
+                    height:"50%"
+                }}>
+                <IconButton>
+                    <Avatar
+                        variant="circular"
+                        sx={{
+                            fontSize: "80px",
+                            width: "auto",
+                            height: "auto"
+                        }}
+                    >
+                        <AddAPhotoIcon style={Object.assign({color: 'grey' })} fontSize='inherit' />
+                    </Avatar>
+
+                </IconButton>
+            </Box>
         </div>
     )
 }
